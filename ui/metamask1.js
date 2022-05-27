0b56953156b0c4db1c5e7c4c5f4b83e2dbe93b25
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-05-27 17:00:43 +0300

    Update metamask1.js
    https://github.com/ilamanov/metamask-react-auth

--- ui/metamask1.js
+++ ui/metamask1.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect,useState } from "react";
 import styles from "./metamask-auth.module.css";
 
 function isMobileDevice() {
